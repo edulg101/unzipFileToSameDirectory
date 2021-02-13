7e49cfab456a27b4df939ae9a19b097c39ca0a9c
Eduardo Lise Gerhardt <62358541+edulg101@users.noreply.github.com>
2021-02-12 21:58:52 -0300

    Add files via upload

---
+++ App2.java
@@ -0,0 +1,113 @@
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipEntry;
+
+public class App {
+
+    public static final int BUFFER_SIZE = 4096;
+
+    public static void main(String[] args) {
+
+        String zippedFilePath = "/home/eduardo/Downloads/compactado";
+        String destPath = "/home/eduardo/Downloads/unziped";
+
+        try {
+            unzipAllDirectory(zippedFilePath, destPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String[] getFilesInPath(String path) throws Exception {
+        File file = null;
+        String[] files = null;
+
+        try {
+            file = new File(path);
+            files = file.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return files;
+    }
+
+    public static void unzipAllDirectory(String path, String destPath) throws IOException {
+        String[] files = null;
+        try {
+            files = getFilesInPath(path);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < files.length; i++) {
+            unzip(path + File.separator + files[i], destPath);
+        }
+    }
+
+    private static void unzip(String zipFilePath, String destDirectory) throws IOException {
+        File destDir = new File(destDirectory);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+        }
+        Charset charset = Charset.forName("UTF-8");
+        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath), charset);
+        ZipEntry entry = zipIn.getNextEntry();
+
+        while (entry != null) {
+
+            String filePath = destDirectory + File.separator + entry.getName();
+            System.out.println("entry.getname: " + entry.getName());
+            String fileName = entry.getName();
+
+            while (fileName.contains(File.separator)) {
+                int index = fileName.indexOf(File.separator);
+                fileName = fileName.substring(index +1);
+                if (fileName.equals("")) {
+                    continue;
+                }
+                filePath = destDirectory + File.separator + fileName;
+                System.out.println(filePath);
+                
+                try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+
+            if (!fileName.contains(".")) {
+                zipIn.closeEntry();
+                entry = zipIn.getNextEntry();
+                continue;
+            }
+
+            extractFile(zipIn, filePath);
+
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+        }
+        zipIn.close();
+    }
+
+    private static void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        File parentFile = new File(filePath).getParentFile();
+        if (parentFile != null && !parentFile.exists()) {
+            parentFile.mkdirs();
+        }
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read = 0;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
+    }
+
+}
